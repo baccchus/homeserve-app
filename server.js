97887155b1bf3f4f1421b2ae79489b46a7a36b30
saraguerric <sara.guerric@evergig.com>
2018-07-04 02:32:01 +0200

    Add: cleanup

--- server.js
+++ server.js
@@ -3,6 +3,5 @@ var app = express();
 
 var port = process.env.PORT || 3000;
 
-//app.get('/', (request, response) => response.sendFile(__dirname +'index.html'));
 app.use(express.static(__dirname));
 app.listen(port);
