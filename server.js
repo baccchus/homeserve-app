beee8b8773574ac3161dd25799ac8d81cad7bf3f
saraguerric <sara.guerric@evergig.com>
2018-07-04 02:21:07 +0200

    Add: server

--- server.js
+++ server.js
@@ -2,5 +2,6 @@ var path = require('path');
 var express = require('express');
 var app = express();
 
+app.get('/', (request, response) => response.sendfile(path.join(__dirname, './index.html')));
 app.use(express.static(path.join(__dirname, '.')));
 app.listen(3000);
